edb36b70b418ac742f60e33cd4ff86c47169d1fd
lharless-mcd <lharless@us.mcd.com>
2021-12-08 11:50:51 -0600

    should work... but doesnt

--- day08/part2.py
+++ day08/part2.py
@@ -28,24 +28,16 @@ def createNumLetterMap(tenNumbersSet):
     }
 
     for item in tenNumbersSet:
-        if len(item) == 2:
-            numLetterMap[item] = 1
-        elif len(item) == 3:
-            numLetterMap[item] = 7
-        elif len(item) == 4:
-            numLetterMap[item] = 4
-        elif len(item) == 7:
-            numLetterMap[item] = 8
-        elif len(item) == 5:
-            tempMap[5].append(set(item.split())) # should turn into char array
+        if len(item) == 5:
+            tempMap[5].append({"og": item, "setVals": set(item)}) # should turn into char array
         elif len(item) == 6:
-            tempMap[6].append(set(item.split())) # should turn into char array
+            tempMap[6].append({"og": item, "setVals": set(item)}) # should turn into char array
 
     def findFive(tempMap):
         for charSetOf5 in tempMap[5]:
             totalSubsets = 0
             for charSetOf6 in tempMap[6]:
-                if charSetOf5.issubset(charSetOf6):
+                if charSetOf5["setVals"].issubset(charSetOf6["setVals"]):
                     totalSubsets += 1
                     if totalSubsets == 2:
                         return charSetOf5
@@ -53,22 +45,54 @@ def createNumLetterMap(tenNumbersSet):
     def findThreeAndNine(tempMap): # Requires 5 to be known and removed
         for charSetOf5 in tempMap[5]:
             for charSetOf6 in tempMap[6]:
-                if charSetOf5.issubset(charSetOf6):
+                if charSetOf5["setVals"].issubset(charSetOf6["setVals"]):
                     return charSetOf5, charSetOf6
 
     def findSix(fiveStringSet, tempMap): # Requires above to be known and removed
         for charSetOf6 in tempMap[6]:
-            if fiveStringSet.issubset(charSetOf6):
-                    return charSetOf6
+            if fiveStringSet["setVals"].issubset(charSetOf6["setVals"]):
+                return charSetOf6
 
+    fiveSet = findFive(tempMap)
+    tempMap[5].remove(fiveSet)
+    threeSet, nineSet = findThreeAndNine(tempMap)
+    tempMap[5].remove(threeSet)
+    tempMap[6].remove(nineSet)
+    sixSet = findSix(fiveSet, tempMap)
+    tempMap[6].remove(sixSet)
+    zeroSet = tempMap[6].pop()
+    twoSet = tempMap[5].pop()
+    # now has objects for all values which include a set and the original
 
+    numLetterMap["".join(sorted(fiveSet["setVals"]))] = 5
+    numLetterMap["".join(sorted(threeSet["setVals"]))] = 3
+    numLetterMap["".join(sorted(nineSet["setVals"]))] = 9
+    numLetterMap["".join(sorted(sixSet["setVals"]))] = 6
+    numLetterMap["".join(sorted(zeroSet["setVals"]))] = 0
+    numLetterMap["".join(sorted(twoSet["setVals"]))] = 2
+
+    return numLetterMap
 
 
 total = 0
-outputSet = set()
+checkCount = 0
 for line in formattedLst:
+    numLetterMap = createNumLetterMap(line["input"])
     for item in line["output"]:
-        if len(item) == 2 or len(item) == 4 or len(item) == 3 or len(item) == 7:
+        checkCount += 1
+        compareItem = "".join(sorted(item))
+        if len(item) == 2:
             total += 1
-            outputSet.add(item)
+        elif len(item) == 3:
+            total += 7
+        elif len(item) == 4:
+            total += 4
+        elif len(item) == 7:
+            total += 8
+        else:
+            total += numLetterMap[compareItem] # All works except that the outputs aren't ordered consistently
+    print(total)
 
+
+print(checkCount)
+print(total)
