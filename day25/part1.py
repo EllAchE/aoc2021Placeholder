fdf5dd803a4c2c65013b052d8de28252f1a3f391
EllAchE <lhar8771@gmail.com>
2022-01-05 17:15:26 -0600

    part1

--- day25/part1.py
+++ day25/part1.py
@@ -4,29 +4,48 @@ lst = [d.strip() for d in data]
 convertedInput = list(map(lambda a : list(a), lst))
 
 # cols = 10, rows = 9
-# cIndex will include ["x pos", y pos, type]
-
-cMap = []
-cIndexLst = []
 
 #################
 # SOLUTION
 #################
-
 # I will be manually calculating each step. I'll maintain a list of cucumber objects and for each "step" I'll check if
 # they can move or not. The east and south steps can be considered separately, but 2 must occur for a counted step.
-# This will iterate until an east and south step both occur with no movement, which would mean no further steps will occur.
+# This will iterate until an east and south step both occur with no movement, which means no further steps will occur.
 # looking at cycles of the different cucumber rows could have a more elegant soln.
 
-def createMapAndList(lst):
-    for inRow in lst:
-        for item in inRow:
+def createCucumberIndexList(convertedInput):
+    cIndexLst = []
+    for i in range(0, len(convertedInput)):
+        for j in range(0, len(convertedInput[0])):
+            if convertedInput[i][j] != ".":
+                if convertedInput[i][j] == ">":
+                    cIndexLst.append({"x": j, "y": i, "type": "east", "canmove": False})
+                elif convertedInput[i][j] == "v":
+                    cIndexLst.append({"x": j, "y": i, "type": "south", "canmove": False})
+                else:
+                    raise AttributeError
 
+    return cIndexLst
 
-def checkIndexList():
+def cucumberStep(cMap, cucumberIndexListIn):
     anyMove = False
-    for cucumber in lst:
-        checkSpot(cucumber, cMap)
+    eastList = list(filter(lambda a: a["type"] == "east", cucumberIndexListIn))
+    southList = list(filter(lambda a: a["type"] == "south", cucumberIndexListIn))
+    for cucumber in eastList:
+        checkIfCanMoveEast(cucumber, cMap)
+    for cucumber in eastList:
+        if cucumber["canmove"]:
+            moveEast(cucumber, cMap)
+            anyMove = True
+
+    for cucumber in southList:
+        checkIfCanMoveSouth(cucumber, cMap)
+    for cucumber in southList:
+        if cucumber["canmove"]:
+            moveSouth(cucumber, cMap)
+            anyMove = True
+
+    return anyMove # flipped my booleans...
 
 def checkIfCanMoveEast(cucumber, cMap):
     if cucumber["x"] == 9: # map edge
@@ -35,7 +54,7 @@ def checkIfCanMoveEast(cucumber, cMap):
         else:
             cucumber["canmove"] = False
     else:
-        if cMap[cucumber["y"]][cucumber["x"]+1] == ".":
+        if cMap[cucumber["y"]][cucumber["x"] + 1] == ".":
             cucumber["canmove"] = True
         else:
             cucumber["canmove"] = False
@@ -52,8 +71,26 @@ def checkIfCanMoveSouth(cucumber, cMap):
         else:
             cucumber["canmove"] = False
 
-def checkSpot(cucumber, cMap):
-    if cucumber["type"] == "east":
-        checkIfCanMoveEast(cucumber, cMap)
-    elif cucumber["type"] == "south":
-        checkIfCanMoveSouth(cucumber, cMap)
+def moveEast(cucumber, cMap):
+    cMap[cucumber["y"]][cucumber["x"]] = "."
+    cucumber["x"] = (cucumber["x"] + 1) % 10
+    cMap[cucumber["y"]][cucumber["x"]] = ">"
+    cucumber["canmove"] = False
+
+def moveSouth(cucumber, cMap):
+    cMap[cucumber["y"]][cucumber["x"]] = "."
+    cucumber["y"] = (cucumber["y"] + 1) % 9
+    cMap[cucumber["y"]][cucumber["x"]] = "v"
+    cucumber["canmove"] = False
+
+
+cucumberIndexList = createCucumberIndexList(convertedInput)
+isNotStuck = True
+stepCount = 0
+while isNotStuck: # does assume at least 1 step
+    print(cucumberIndexList)
+    isNotStuck = cucumberStep(convertedInput, cucumberIndexList)
+    print(cucumberIndexList)
+    stepCount += 1
+
+print(stepCount)
