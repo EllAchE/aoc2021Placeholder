ccee18dd2fc999b8a2c26d54d44ce499a98241a9
lharless-mcd <lharless@us.mcd.com>
2022-01-11 09:26:22 -0600

    break up input

--- day24/part1.py
+++ day24/part1.py
@@ -2,8 +2,23 @@ data = open("input.txt").readlines()
 # copy the data to a list
 lst = [d.strip() for d in data]
 
+# Quickly wrote a brute force. That doesn't work, so think I'll need to reverse the steps
+# i.e. start from z + y must = 0
+
+def memoize(func):
+    cache = dict()
+
+    def memoized_func(*args):
+        if args in cache:
+            return cache[args]
+        result = func(*args)
+        cache[args] = result
+        return result
+
+    return memoized_func
+
 def inputCom(wxyz, varOne, initialInteger):
-    wxyz[varOne] = initialInteger.pop()
+    wxyz[varOne] = initialInteger.pop(0)
     return wxyz
 
 def addCom(wxyz, varOne, varTwo):
@@ -11,11 +26,12 @@ def addCom(wxyz, varOne, varTwo):
     return wxyz
 
 def mulCom(wxyz, varOne, varTwo):
-    wxyz[varOne] = wxyz[varOne] * varTwo
+    res = wxyz[varOne] * varTwo
+    wxyz[varOne] = res
     return wxyz
 
 def divCom(wxyz, varOne, varTwo):
-    wxyz[varOne] = wxyz[varOne] / varTwo
+    wxyz[varOne] = int(wxyz[varOne] / varTwo)
     return wxyz
 
 def modCom(wxyz, varOne, varTwo):
@@ -29,12 +45,16 @@ def eqlCom(wxyz, varOne, varTwo):
         wxyz[varOne] = 0
     return wxyz
 
+def setVarCom(wxyz, varOne, varTwo):
+    wxyz[varOne] = varTwo
+    return wxyz
+
 def parseInstruction(iptLine, wxyz, monadCode):
     comm = iptLine[:3]
     varOne = iptLine[4]
 
     if len(iptLine) >= 7:
-        if iptLine[6] in ['w', 'x', 'y', 'z']:
+        if iptLine[6] in ["w", "x", "y", "z"]:
             varTwo = wxyz[iptLine[6]]
         else:
             varTwo = int(iptLine[6:])
@@ -51,19 +71,22 @@ def parseInstruction(iptLine, wxyz, monadCode):
         wxyz = divCom(wxyz, varOne, varTwo)
     elif comm == "mod":
         wxyz = modCom(wxyz, varOne, varTwo)
+    elif comm == "set":
+        wxyz = modCom(wxyz, varOne, varTwo)
 
     return wxyz
 
 def checkMonad(monadCode):
     wxyz = {
-        "w": 0,
-        "x": 0,
-        "y": 0,
-        "z": 0
+        'w': 0,
+        'x': 0,
+        'y': 0,
+        'z': 0
     }
 
     for line in lst:
         wxyz = parseInstruction(line, wxyz, monadCode)
+    print(wxyz)
 
     if wxyz["z"] == 0:
         return False
@@ -83,9 +106,7 @@ def incrementIntList(intList):
     return intList
 
 
-initialInteger = [9, 9, 9, 9, 9,
-                  9, 9, 9, 9, 9,
-                  9, 9, 9, 10]
+initialInteger = [9] * 14
 
 shouldContinue = True
 i = 0
